63507873bd3f6240f163f6b5ca1e7977e04dab4a
mazhar <mazharsalmi@gmail.com>
2022-04-29 16:59:35 +0500

    responsive style

--- src/Pages/Teacher.js
+++ src/Pages/Teacher.js
@@ -2,16 +2,18 @@ import React from 'react'
 import teacher  from "../Pages/Teacher.css"
 import {Grid ,Checkbox } from "@mui/material"
 import TeacherCalender from "../Table/TeacherCalender"
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 export default function Prepare() {
   return (
     <div className='prepare'>
     <Grid container spacing={2} pt='3rem'>
         <Grid item xs={12} md={2}>
         
-        <ul className='list-of-teacher'>
-        <li>Teacher</li>
-        <li>Subject</li>
-        <li>Class</li>
+        <ul className='list-of-teacher'>Class List
+        <li><Checkbox  defaultChecked /> Augumented Reality</li>
+        <li><Checkbox  defaultChecked /> Programming</li>
+        <li><Checkbox  defaultChecked /> Data Structures</li>
+        <li><Checkbox  defaultChecked /> Machine Learning</li>
         </ul>
         
         </Grid>
